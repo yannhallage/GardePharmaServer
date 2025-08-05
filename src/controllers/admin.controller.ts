47b1707e2cfc5449fda054f32160e5195606ca86
yannhallage <contact.devhllg@gmail.com>
2025-08-05 06:00:39 +0000

    developpement des premiers routes (admin)

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import AdminService from '../services/admin.service';
+import gardeService from '../services/garde.service'
 
 export const createAdmin = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -7,5 +8,41 @@ export const createAdmin = async (req: Request, res: Response, next: NextFunctio
         res.status(201).json(newAdmin);
     } catch (err) {
         next(err);
+    }
+};
+
+
+export const getAllHistoriqueGardes = async (req: Request, res: Response) => {
+    try {
+        const gardes = await gardeService.getAllHistoriqueByAdmin();
+
+        return res.status(200).json({
+            success: true,
+            message: 'Historique des gardes récupéré avec succès',
+            data: gardes,
+        });
+    } catch (error) {
+        console.error('[Erreur Historique Gardes]', error);
+        return res.status(500).json({
+            success: false,
+            message: 'Erreur lors de la récupération des gardes',
+        });
+    }
+};
+export const getAllGardes = async (req: Request, res: Response) => {
+    try {
+        const gardes = await gardeService.getAllGardeByAdmin();
+
+        return res.status(200).json({
+            success: true,
+            message: 'toutes les gardes récupéré avec succès',
+            data: gardes,
+        });
+    } catch (error) {
+        console.error('[Erreur Historique Gardes]', error);
+        return res.status(500).json({
+            success: false,
+            message: 'Erreur lors de la récupération des gardes',
+        });
     }
 };
--- src/services/garde.service.ts
+++ src/services/garde.service.ts
@@ -5,14 +5,21 @@ export const getHistoriqueByPharmacyId = async (pharmacyId: string): Promise<IGa
     if (!mongoose.Types.ObjectId.isValid(pharmacyId)) {
         return [];
     }
-
-    // return await Garde.find({
-    //     reference: pharmacyId,
-    //     statut: 'validée',
-    // });
     return await Garde.find({
         userId: pharmacyId,
         statut: { $in: ['validée', 'refusée'] },
+    });
+};
+
+export const getAllHistoriqueByAdmin = async (): Promise<IGarde[]> => {
+    return await Garde.find({
+        statut: { $in: ['validée', 'refusée'] },
+    });
+};
+
+export const getAllGardeByAdmin = async (): Promise<IGarde[]> => {
+    return await Garde.find({
+        statut: { $in: ['en cours', 'en attente'] },
     });
 };
 
@@ -36,5 +43,7 @@ export const createGarde = async (data: Partial<IGarde>): Promise<IGarde> => {
 export default {
     getHistoriqueByPharmacyId,
     getConsulterGardeByPharmacyId,
-    createGarde
+    createGarde,
+    getAllHistoriqueByAdmin,
+    getAllGardeByAdmin
 };
